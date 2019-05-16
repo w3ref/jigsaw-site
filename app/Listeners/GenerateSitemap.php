7b574df0643c75155f4121a8ab88f56933cbba46
Keith Damiani <keith@tighten.co>
2019-05-16 10:03:56 -0400

    Fix slashes in sitemap for top-level pages

--- app/Listeners/GenerateSitemap.php
+++ app/Listeners/GenerateSitemap.php
@@ -30,7 +30,7 @@ class GenerateSitemap
             ->reject(function ($path) {
                 return $this->isExcluded($path);
             })->each(function ($path) use ($baseUrl, $sitemap) {
-                $sitemap->addItem(rtrim($baseUrl, '/') . $path, time(), Sitemap::DAILY);
+                $sitemap->addItem(trim(rtrim($baseUrl, '/') . '/' . ltrim($path, '/'), '/'), time(), Sitemap::DAILY);
         });
 
         $sitemap->write();
