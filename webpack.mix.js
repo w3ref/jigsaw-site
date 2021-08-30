e3325bab2db7e6e86a564e6d9e8bd7d768dfbf35
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-08-30 14:04:29 -0400

    Convert Less to vanilla CSS (with PostCSS)

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,16 +7,13 @@ mix.setPublicPath('source/assets/build');
 
 mix.js('source/_assets/js/app.js', 'js')
     .vue()
-    .less('source/_assets/less/main.less', 'css')
+    .css('source/_assets/css/app.css', 'css', [require('tailwindcss')])
     .jigsaw()
-    .options({
-        postCss: [require('tailwindcss')],
-    })
-    .purgeCss({
-        enabled: mix.inProduction(),
-        content: 'source/**/*.{html,js,php,md,vue}',
-        safelistPatterns: [/language/, /hljs/, /algolia/, /docsearch/, /ds-/],
-    })
+    // .purgeCss({
+    //     enabled: mix.inProduction(),
+    //     content: 'source/**/*.{html,js,php,md,vue}',
+    //     safelistPatterns: [/language/, /hljs/, /algolia/, /docsearch/, /ds-/],
+    // })
     .browserSync({
         server: 'build_local',
         files: ['build_local/**'],
